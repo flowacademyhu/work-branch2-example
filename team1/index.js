d4babb5fc39732a9eefb7049683b331a70f615ce
Zsolt Tasnadi <rastasi@gmail.com>
2020-05-16 13:50:11 +0200

    add team1

---
+++ team1/index.js
@@ -0,0 +1,26 @@
+// This lovely function will, as a well functioning democracy, divide things in accordance with it's master's plan. Divide a with b.
+
+const div = (a, b) => {
+  return a / b;
+};
+
+// sub
+const sub = (a, b) => {
+  return a - b;
+};
+
+// mul
+const mul = (a, b) => {
+  return a * b;
+};
+// sum  function
+const sum = (a, b) => {
+  return a + b;
+};
+
+module.exports = {
+  sum: sum,
+  mul: mul,
+  div: div,
+  sub: sub
+};
