992fa461a95d83f275ade1fb20c82f7bb3e0bee4
Zsolt Tasnadi <rastasi@gmail.com>
2020-05-16 13:53:35 +0200

    add team3

---
+++ team3/index.js
@@ -0,0 +1,23 @@
+// adding comment -gy
+
+const sum = (a, b) => { // writen by Dani
+  return a + b;
+};
+const sub = (a, b) => { // writen by Dani
+  return a - b;
+};
+
+const mul = (a, b) => { // writen by gy
+  return a * b;
+};
+
+const div = (a, b) => { // writen by gy
+  return a / b;
+};
+
+module.exports = {
+  sum,
+  sub,
+  mul,
+  div
+};
